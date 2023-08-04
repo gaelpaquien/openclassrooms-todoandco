6f63d381cebdb4ec84ffe51612245f07dff7e95a
Galuss <galuss.contact@gmail.com>
2023-08-04 13:56:08 +0200

    (#9 & #10): Overall improvement (upgrade) of the application

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Service\TaskService;
+use App\Service\UserService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TaskController extends AbstractController
+{
+    public function __construct(private TaskService $taskService, private UserService $userService)
+    {
+        $this->taskService = $taskService;
+        $this->userService = $userService;
+    }
+
+    #[Route('/tasks', name: 'task_list')]
+    public function listAction(): Response
+    {
+        return $this->render('task/list.html.twig', ['tasks' => $this->taskService->list()]);
+    }
+
+    #[Route('/tasks/create', name: 'task_create')]
+    public function createAction(Request $request): Response
+    {
+        // Check if the user is logged in
+        if (!$this->userService->userIsLogged()) {
+            $this->addFlash('error', 'Vous devez être connecté pour créer une tâche.');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->taskService->create($task);
+
+            $this->addFlash('success', 'La tâche a été bien été ajoutée.');
+
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->render('task/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route('/tasks/{id}/edit', name: 'task_edit')]
+    public function editAction(Task $task, Request $request): Response
+    {
+        // Check if the user is logged in
+        if (!$this->userService->userIsLogged()) {
+            $this->addFlash('error', 'Vous devez être connecté pour modifier une tâche.');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        // If task author is not anonymous, check if the user is the author of the task
+        if (!$this->taskService->authorIsAnonymous($task)) {
+            if (!$this->taskService->userIsAuthor($task)) {
+                $this->addFlash('error', 'Vous ne pouvez pas modifier une tâche dont vous n\'êtes pas l\'auteur.');
+
+                return $this->redirectToRoute('task_list');
+            }
+        }
+
+        // If task author is anonymous, check if the user is admin
+        if ($this->taskService->authorIsAnonymous($task)) {
+            if (!$this->userService->userIsAdmin($this->getUser())) {
+                $this->addFlash('error', 'Seul un administrateur peut modifier une tâche anonyme.');
+
+                return $this->redirectToRoute('task_list');
+            }
+        }
+
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->taskService->edit();
+
+            $this->addFlash('success', 'La tâche a bien été modifiée.');
+
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->render('task/edit.html.twig', [
+            'form' => $form->createView(),
+            'task' => $task,
+        ]);
+    }
+
+    #[Route('/tasks/{id}/toggle', name: 'task_toggle')]
+    public function toggleTaskAction(Task $task): Response
+    {
+        // Check if the user is logged in
+        if (!$this->userService->userIsLogged()) {
+            $this->addFlash('error', 'Vous devez être connecté pour modifier l\'état d\'une tâche.');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        // If task author is not anonymous, check if the user is the author of the task
+        if (!$this->taskService->authorIsAnonymous($task)) {
+            if (!$this->taskService->userIsAuthor($task)) {
+                $this->addFlash('error', 'Vous ne pouvez pas modifier l\'état d\'une tâche dont vous n\'êtes pas l\'auteur.');
+
+                return $this->redirectToRoute('task_list');
+            }
+            $this->addFlash('error', 'Vous ne pouvez pas modifier cette tâche.');
+        }
+
+        // If task author is anonymous, check if the user is admin
+        if ($this->taskService->authorIsAnonymous($task)) {
+            if (!$this->userService->userIsAdmin($this->getUser())) {
+                $this->addFlash('error', 'Seul un administrateur peut modifier l\'état d\'une tâche dont l\'auteur est anonyme.');
+
+                return $this->redirectToRoute('task_list');
+            }
+        }
+
+        $this->taskService->toggle($task);
+
+        if ($task->isDone()) {
+            $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+        }
+
+        if (!$task->isDone()) {
+            $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme non terminée.', $task->getTitle()));
+        }
+
+        return $this->redirectToRoute('task_list');
+    }
+
+    #[Route('/tasks/{id}/delete', name: 'task_delete')]
+    public function deleteTaskAction(Task $task): Response
+    {
+        // Check if the user is logged in
+        if (!$this->userService->userIsLogged()) {
+            $this->addFlash('error', 'Vous devez être connecté pour supprimer une tâche.');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        // If task author is not anonymous, check if the user is the author of the task
+        if (!$this->taskService->authorIsAnonymous($task)) {
+            if (!$this->taskService->userIsAuthor($task)) {
+                $this->addFlash('error', 'Vous ne pouvez pas supprimer une tâche dont vous n\'êtes pas l\'auteur.');
+
+                return $this->redirectToRoute('task_list');
+            }
+        }
+
+        // If task author is anonymous, check if the user is admin
+        if ($this->taskService->authorIsAnonymous($task)) {
+            if (!$this->userService->userIsAdmin($this->getUser())) {
+                $this->addFlash('error', 'Seul un administrateur peut supprimer une tâche dont l\'auteur est anonyme.');
+
+                return $this->redirectToRoute('task_list');
+            }
+        }
+
+        $this->taskService->delete($task);
+
+        $this->addFlash('success', 'La tâche a bien été supprimée.');
+
+        return $this->redirectToRoute('task_list');
+    }
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Service\UserService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    public function __construct(private UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    #[Route('/users', name: 'user_list')]
+    public function listAction(): Response
+    {
+        // Check if user is logged in
+        if (!$this->userService->userIsLogged()) {
+            $this->addFlash('error', 'Vous devez être connecté pour accéder à cette page.');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        // Check if user is admin
+        if (!$this->userService->userIsAdmin($this->getUser())) {
+            $this->addFlash('error', 'Vous ne pouvez pas accéder à cette page.');
+
+            return $this->redirectToRoute('app_default');
+        }
+
+        return $this->render('user/list.html.twig', ['users' => $this->userService->list()]);
+    }
+
+    #[Route('/users/create', name: 'user_create')]
+    public function createAction(Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Check if email already exists
+            if ($this->userService->emailExist($form->get('email')->getData())) {
+                $this->addFlash('error', 'Cette adresse email est déjà utilisée.');
+
+                return $this->redirectToRoute('user_create');
+            }
+
+            $this->userService->create($form, $userPasswordHasher, $user);
+
+            $this->addFlash('success', "Votre compte vient d'être créer avec succès.");
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('user/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route('/users/{id}/edit', name: 'user_edit')]
+    public function editAction(User $user, Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
+    {
+        // Check if the user is logged in
+        if (!$this->userService->userIsLogged()) {
+            $this->addFlash('error', 'Vous devez être connecté pour modifier un compte.');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        // Check if user is the same as the logged in or if the user is admin
+        if (!$this->userService->userIsAdmin($this->getUser()) && !$this->userService->isSameUser($user)) {
+            $this->addFlash('error', 'Vous ne pouvez pas modifier ce compte.');
+
+            return $this->redirectToRoute('app_default');
+        }
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Check if email already exists and if it's not the same as the user's email
+            if ($this->userService->emailExist($form->get('email')->getData()) && !$this->userService->isSameEmail($form->get('email')->getData(), $user)) {
+                $this->addFlash('error', 'Cette adresse email est déjà utilisée.');
+
+                return $this->redirectToRoute('user_edit', ['id' => $user->getId()]);
+            }
+
+            $this->userService->edit($form, $userPasswordHasher, $user);
+
+            $this->addFlash('success', "Le compte a bien été modifié.");
+
+            return $this->redirectToRoute('app_default');
+        }
+
+        return $this->render('user/edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
+    }
+}
