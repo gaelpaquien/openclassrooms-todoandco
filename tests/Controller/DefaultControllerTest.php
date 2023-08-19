66736b459612905ec5be5564b03a612da01cfd7e
Gaël Paquien <contact@gaelpaquien.com>
2023-08-19 20:54:42 +0200

    (#2): Code quality audit (Fix Codacy)

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -11,7 +11,7 @@ class DefaultControllerTest extends WebTestCase
     private KernelBrowser|null $client = null;
     private $urlGenerator;
 
-    public function setUp() : void
+    public function setUp(): void
     {
         $this->client = static::createClient();
         $this->urlGenerator = $this->client->getContainer()->get('router.default');
