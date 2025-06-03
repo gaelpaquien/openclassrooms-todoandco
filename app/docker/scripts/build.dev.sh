396e67e7e9144f52ac4dd4e4080e4ab8bdc5d815
Gaël Paquien <gael@plany.fr>
2025-06-03 21:25:13 +0200

    Improve env + Prepares for production

---
+++ app/docker/scripts/build.dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /var/www
+
+echo "Starting building app in DEV environment..."
+
+echo "Installing dependencies..."
+composer install || exit 1
+
+echo "Setting up database...."
+php bin/console doctrine:database:create --env=dev --if-not-exists
+php bin/console doctrine:schema:create --env=dev
+echo "yes" | php bin/console doctrine:fixtures:load --env=dev
+echo "yes" | php bin/console doctrine:fixtures:load --env=dev
+
+echo "Clearing cache..."
+symfony console cache:clear --env=dev || exit 1
+
+echo "Building app completed successfully!"
+
+exec "$@" || exit 1
