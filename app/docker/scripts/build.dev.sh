626eba52acccb7c2d61e63387145ff5c8998186c
Gaël Paquien <gael@plany.fr>
2025-06-04 13:50:40 +0200

    Add docker login + Fix deploy scripts

--- app/docker/scripts/build.dev.sh
+++ app/docker/scripts/build.dev.sh
@@ -8,13 +8,13 @@ echo "Installing dependencies..."
 composer install || exit 1
 
 echo "Setting up database...."
-php bin/console doctrine:database:create --env=dev --if-not-exists
-php bin/console doctrine:schema:create --env=dev
-echo "yes" | php bin/console doctrine:fixtures:load --env=dev
+php bin/console doctrine:database:create --env=dev --if-not-exists || exit 1
+php bin/console doctrine:schema:create --env=dev || exit 1
+echo "yes" | php bin/console doctrine:fixtures:load --env=dev || exit 1
 
 echo "Clearing cache..."
 symfony console cache:clear --env=dev || exit 1
 
 echo "Building app completed successfully!"
 
-exec "$@" || exit 1
+exec "$@"
--- app/docker/scripts/build.prod.sh
+++ app/docker/scripts/build.prod.sh
@@ -16,9 +16,9 @@ echo "Optimizing environment variables..."
 composer dump-env prod || exit 1
 
 echo "Setting up database...."
-php bin/console doctrine:database:create --env=prod --if-not-exists --no-interaction
-php bin/console doctrine:schema:update --env=prod --force --no-interaction
-php bin/console doctrine:migrations:migrate --env=prod --no-interaction
+php bin/console doctrine:database:create --env=prod --if-not-exists --no-interaction || exit 1
+php bin/console doctrine:schema:update --env=prod --force --no-interaction || exit 1
+php bin/console doctrine:migrations:migrate --env=prod --no-interaction || exit 1
 
 echo "Clearing cache..."
 php bin/console cache:clear --env=prod --no-interaction || exit 1
