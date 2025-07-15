c42b33633f3ea83e565451a96a476a6d0f74d86e
Gaël Paquien <contact@gaelpaquien.com>
2025-07-15 16:08:36 +0200

    Fix build prod script

--- app/docker/scripts/build.prod.sh
+++ app/docker/scripts/build.prod.sh
@@ -16,8 +16,9 @@ echo "Optimizing environment variables..."
 composer dump-env prod || exit 1
 
 echo "Setting up database...."
-php bin/console doctrine:database:create --env=prod --if-not-exists --no-interaction || exit 1
-php bin/console doctrine:schema:update --env=prod --force --no-interaction || exit 1
+php bin/console doctrine:database:drop --env=prod --force --if-exists --no-interaction || exit 1
+php bin/console doctrine:database:create --env=prod --no-interaction || exit 1
+php bin/console doctrine:schema:update --force --env=prod || exit 1
 php bin/console doctrine:migrations:migrate --env=prod --no-interaction || exit 1
 
 echo "Clearing cache..."
